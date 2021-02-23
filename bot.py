b5f7cda35f6a008a5df127bfdf952484a17655f2
kkqu <48061679+kkqu@users.noreply.github.com>
2021-02-23 09:42:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,40 +2,59 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import threading
-from random import random, randint
-import os
-
+from random import randint
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 # User Variables
-UserEmail = os.environ.get('EMAIL') #insert your password in this variable
-UserPassword = os.environ.get('DISCORDPASSWORD') #insert your password in this variable
-Serverlink = "https://discord.com/channels/809044507194294312/809044716544458782"
-Textxpath = "/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/main/form/div/div/div/div/div/div[3]/div[2]"
-fricklist = ["f", "r", "i", "c", "k"]
+UserEmail = ""
+UserPassword = ""
+Serverlink = ""
+Textxpath = "/html/body/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div/div[3]/div[2]/div"
 
 # Opening discord link
-driver = webdriver.Chrome() #insert your path into those brackets
+driver = webdriver.Chrome(PATH)
 driver.get(Serverlink)
 
 # Logging in
 email = driver.find_element_by_name("email")
 email.send_keys(UserEmail)
 password = driver.find_element_by_name("password")
-password.send_keys(UserPassword)
+password.send_keys(UserPassword)    
 login = driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]")
 login.click()
 time.sleep(5)
 
+#list of meme types (postmeme)
+fricklist = ["f", "r", "i", "c", "k"]
+
 chat = driver.find_element_by_xpath(Textxpath)
 
 # Sending pls beg every 45 seconds
-def beg():    
+
+def beg():
     while True:
         chat.send_keys("pls beg")
         chat.send_keys(Keys.ENTER)
-        time.sleep(45)
+        time.sleep(48)
+# Sending pls fish every 60 seconds
+# Sending pls hunt every 60 seconds
 
+def fishunt():
+    while True:
+        chat.send_keys("pls fish")
+        chat.send_keys(Keys.ENTER)
+        chat.send_keys("pls hunt")
+        chat.send_keys(Keys.ENTER)
+        time.sleep(63)
+# Sending pls daily every 24 hour
+        
+def daily():
+    while True:
+        chat.send_keys("pls daily")
+        chat.send_keys(Keys.ENTER)
+        time.sleep(86405)
+# Sending pls postmeme every 60 seconds and randomly choosing a meme
+    
 def frick():
-    time.sleep(1)
     while True:
         rand = randint(0, len(fricklist)-1)
         chat.send_keys("pls pm")
@@ -44,21 +63,38 @@ def frick():
         chat.send_keys(fricklist[rand])
         chat.send_keys(Keys.ENTER)
         time.sleep(63)
-def fishunt():
-    time.sleep(3)
-    while True:
-        chat.send_keys("pls hunt")
-        chat.send_keys(Keys.ENTER)
-        chat.send_keys("pls fish")
-        chat.send_keys(Keys.ENTER)
-        time.sleep(64)
-
+# Depositing all money to the bank every hour
+        
 def deposit():
-    time.sleep(4)
     while True:
         chat.send_keys("pls dep all")
         chat.send_keys(Keys.ENTER)
-        time.sleep(3600)
+        time.sleep(600)
+# Taking care of your pet
+
+def pet():
+    while True:
+        for i in range(3):
+            chat.send_keys("pls pet feed")
+            chat.send_keys(Keys.ENTER)
+        for i in range(2):
+            chat.send_keys("pls pet wash")
+            chat.send_keys(Keys.ENTER)
+        time.sleep(86410)
+# Failing your work every 30 minutes
+
+def job():
+    while True:
+        chat.send_keys("pls work")
+        chat.send_keys(Keys.ENTER)
+        time.sleep(2)
+        for i in range(3):
+            chat.send_keys("IgnacyNoob")
+            chat.send_keys(Keys.ENTER)
+        time.sleep(1800)
+# Sending pls search every 30 seconds
+
+# Threading beg fishunt frick deposit job
 
 thread1 = threading.Thread(target = beg)
 thread1.start()
